4947a2ceb9eabbc093d8ecb8bf5bd02272897c93
Andrew Hill <andrew.hill@infoworks-tn.com>
2020-10-30 17:51:26 -0500

    Email name change

--- visualstudio/NewBeginningsSurvey/TNBCSurvey/Service/EmailService.cs
+++ visualstudio/NewBeginningsSurvey/TNBCSurvey/Service/EmailService.cs
@@ -29,10 +29,27 @@ namespace TNBCSurvey.Service
             //client.UseDefaultCredentials = true;
             //client.Send(objeto_mail);
 
+            MailMessage objeto_mail = new MailMessage();
+            SmtpClient client = new SmtpClient();
+            client.Port = 587;
+            client.Host = "smtp.gmail.com";
+            client.Timeout = 10000;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = true;
+            client.EnableSsl = true;
+            client.Credentials = new NetworkCredential("andrewparttwo@gmail.com", "moonimoonio0");
+            objeto_mail.From = new MailAddress("NoReply@tnbcenter.org", "The New Beginnings Center");
+            objeto_mail.To.Add(new MailAddress(receiver));
+            objeto_mail.IsBodyHtml = true;
+            objeto_mail.Subject = subject;
+            objeto_mail.Body = msg;
+            client.Send(objeto_mail);
+
+
             //MailMessage objeto_mail = new MailMessage();
             //SmtpClient client = new SmtpClient();
             //client.Port = 587;
-            //client.Host = "smtp.gmail.com";
+            //client.Host = "smtp-relay.gmail.com";
             //client.Timeout = 10000;
             //client.DeliveryMethod = SmtpDeliveryMethod.Network;
             //client.UseDefaultCredentials = true;
@@ -43,22 +60,6 @@ namespace TNBCSurvey.Service
             //objeto_mail.Subject = subject;
             //objeto_mail.Body = msg;
             //client.Send(objeto_mail);
-
-
-            MailMessage objeto_mail = new MailMessage();
-            SmtpClient client = new SmtpClient();
-            client.Port = 587;
-            client.Host = "smtp-relay.gmail.com";
-            client.Timeout = 10000;
-            client.DeliveryMethod = SmtpDeliveryMethod.Network;
-            client.UseDefaultCredentials = true;
-            client.EnableSsl = true;
-            client.Credentials = new NetworkCredential("andrewparttwo@gmail.com", "moonimoonio0");
-            objeto_mail.From = new MailAddress("andrewparttwo@gmail.com", "Andrew Hill");
-            objeto_mail.To.Add(new MailAddress(receiver));
-            objeto_mail.Subject = subject;
-            objeto_mail.Body = msg;
-            client.Send(objeto_mail);
         }
     }
 }
