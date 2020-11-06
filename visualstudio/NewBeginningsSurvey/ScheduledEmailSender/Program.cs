f4b6712f4636a35936a791ce10dae073deb3b3d7
Andrew Hill <andrew.hill@infoworks-tn.com>
2020-11-05 20:02:02 -0600

    Email configuration update

--- visualstudio/NewBeginningsSurvey/ScheduledEmailSender/Program.cs
+++ visualstudio/NewBeginningsSurvey/ScheduledEmailSender/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
@@ -82,8 +83,8 @@ namespace ScheduledEmailSender
 ";
         static void Main(string[] args)
         {
-            SqlConnection DBConnection =
-                new SqlConnection("Data Source=newbeginningserver.database.windows.net;Initial Catalog=NewBeginningDB;User ID=newbegininingadmin;PWD=Portal1!;");
+            var defaultConnection = ConfigurationManager.AppSettings["DefaultConnection"];
+            SqlConnection DBConnection = new SqlConnection(defaultConnection);
             string sql = @"exec dbo.makeBatchTickets";
             SqlCommand cmd = new SqlCommand(sql, DBConnection);
             cmd.CommandType = CommandType.Text;
@@ -115,16 +116,20 @@ namespace ScheduledEmailSender
 
         public void sendMail(string subject, string msg, string receiver)
         {
+            var emailHost = ConfigurationManager.AppSettings["emailHost"];
+            var emailUsername = ConfigurationManager.AppSettings["emailUsername"];
+            var emailPassword = ConfigurationManager.AppSettings["emailPassword"];
+
             MailMessage objeto_mail = new MailMessage();
             SmtpClient client = new SmtpClient();
             client.Port = 587;
-            client.Host = "smtp.gmail.com";
+            client.Host = emailHost;
             client.Timeout = 10000;
             client.DeliveryMethod = SmtpDeliveryMethod.Network;
             client.UseDefaultCredentials = true;
             client.EnableSsl = true;
-            client.Credentials = new NetworkCredential("andrewparttwo@gmail.com", "moonimoonio0");
-            objeto_mail.From = new MailAddress("NoReply@tnbcenter.org", "The New Beginnings Center");
+            client.Credentials = new NetworkCredential(emailUsername, emailPassword);
+            objeto_mail.From = new MailAddress(emailUsername, "The New Beginnings Center");
             objeto_mail.To.Add(new MailAddress(receiver));
             objeto_mail.IsBodyHtml = true;
             objeto_mail.Subject = subject;
